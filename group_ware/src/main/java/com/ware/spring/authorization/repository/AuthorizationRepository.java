1efdc44149e3980a9db1088a69c0ee0d6af20f7c
soonuki <soonuki99@gmail.com>
2024-09-04 09:48:58 +0900

    홍순욱 드라이브

--- group_ware/src/main/java/com/ware/spring/authorization/repository/AuthorizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ware.spring.authorization.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.ware.spring.authorization.domain.Authorization;
-
-public interface AuthorizationRepository extends JpaRepository<Authorization, Long> {
-
-	Authorization findByAuthorNo(Long AuthorNo);
-}
